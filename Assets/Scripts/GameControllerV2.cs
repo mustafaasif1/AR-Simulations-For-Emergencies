16ef6535c29780d091310df646f597f38bfba5d8
Mustafa Asif <mustafaasif1@hotmail.com>
2021-01-15 21:17:54 +0500

    Added animation of bring extinguisher to camera

--- Assets/Scripts/GameControllerV2.cs
+++ Assets/Scripts/GameControllerV2.cs
@@ -9,6 +9,7 @@ public class GameControllerV2 : MonoBehaviour
     public GameObject Canvas;
     public GameObject FireExtinguisher;
     public GameObject Dustbin;
+    public GameObject Cam;
     
     // public GameObject Camera;
     
@@ -27,13 +28,18 @@ public class GameControllerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          if (Input.GetMouseButtonDown (0)) {
-        
-        RaycastHit hitInfo = new RaycastHit ();
-        if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
-            Debug.Log ("Object Hit is " + hitInfo.collider.gameObject.name);
+        if (Input.GetMouseButtonDown (0)) 
+        {
+            RaycastHit hitInfo = new RaycastHit ();
+            if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) 
+            {
+                Debug.Log ("Object Hit is " + hitInfo.collider.gameObject.name);
+                if (hitInfo.collider.gameObject.name == "Fire Extinguisher")
+                {
+                    FireExtinguisher.transform.parent = Cam.transform;
+                    FireExtinguisher.GetComponent<Animator>().Play("Bring Extinguisher to Camera");
+                }
 
-            
             } 
         }
     }
