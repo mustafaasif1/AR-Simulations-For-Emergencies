a9adaf8e3873ce312da8a3c19115968ef9a9a392
Maroof Saleemi <Maroof@192.168.10.6>
2021-01-18 23:33:57 +0500

    colliding with wait

--- Assets/SmokeScript.cs
+++ Assets/SmokeScript.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class SmokeScript : MonoBehaviour
 {
     public int collisions;
-     public static bool colliding;
+    public static bool colliding;
+    public int frames;
+    public int curr_frames;
 
     void Start(){
         collisions = 0;
         colliding = false;
+        frames = 0;
+        curr_frames = 0;
+
     }
 
     void LateUpdate(){
+        if (colliding){
+            curr_frames = frames;
+        }
         colliding = false;
     }
 
     void Update(){
+        frames += 1;
         if(!colliding){
-            collisions = 0;
+            if ((frames - curr_frames)>5){
+                collisions = 0;
+                }
             }
     }
 
@@ -29,8 +40,10 @@ public class SmokeScript : MonoBehaviour
        collisions += 1;
        colliding = true;
     }
-
-       Debug.Log(collisions + "itni hain");
+    Debug.Log(collisions+"times hit");
+    if(collisions > 120){
+        GameObject.Find("PS_Parent").GetComponent<ParticleSystem>().Stop();
+    }
 
     }
 }
