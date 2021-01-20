1e2f848f114660fe5f0d4ec320ae06fa978eca8a
omershakeel <21100193@lums.edu.pk>
2021-01-21 02:45:38 +0500

    timer

--- Assets/Models/Camera/CameraScript.cs
+++ Assets/Models/Camera/CameraScript.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject Camera;
-    
 
     // Update is called once per frame
     void Update()
     {
-        float speed = 4.0f;
-        var velocidade = 30;
+        if(GameControllerV2.on){
+            float speed = 4.0f;
+            var velocidade = 30;
 
-        var move = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
-        transform.position += move * speed * Time.deltaTime;
+            var move = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+            transform.position += move * speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.N))
-        {
-            transform.Rotate(Vector3.up * velocidade * Time.deltaTime); 
-        }
-        
-        if (Input.GetKey(KeyCode.M))
-        {
-            transform.Rotate(-Vector3.up * velocidade * Time.deltaTime);
+            if (Input.GetKey(KeyCode.N))
+            {
+                transform.Rotate(Vector3.up * velocidade * Time.deltaTime); 
+            }
+            
+            if (Input.GetKey(KeyCode.M))
+            {
+                transform.Rotate(-Vector3.up * velocidade * Time.deltaTime);
+            }
         }
     }
 
--- Assets/Scripts/GameControllerV2.cs
+++ Assets/Scripts/GameControllerV2.cs
@@ -19,12 +19,18 @@ public class GameControllerV2 : MonoBehaviour
     public Scene StartingScene;
 
     public TextMeshProUGUI message;
+    public TextMeshProUGUI TimeCount;
+
+    public int TimeRemaining;
+    public float TimeTrack;
 
 
     bool PinIsRemoved = false;
     bool ExtinguisherInFrontOfCamera = false;
     bool aimed = false;
     bool firing = false;
+    bool fireAlarmActivated = false;
+    public static bool on;
     
     // Start is called before the first frame update
     void Start()
@@ -33,82 +39,102 @@ public class GameControllerV2 : MonoBehaviour
         StartingScene = SceneManager.GetActiveScene();
         Reset.onClick.AddListener(ResetClick);
         FireExtinguisher.transform.Find("polySurface10").gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-        AlarmSound.SetActive(false);           
+        AlarmSound.SetActive(false); 
+        TimeRemaining = 120;
+        TimeTrack = 120;
+        TimeCount.text = TimeRemaining.ToString();
+        on = true;         
         
     }
 
     // Update is called once per frame
+    void FixedUpdate(){
+        TimeTrack -= 1/50f;
+        TimeRemaining = (int)TimeTrack;
+        int minutes = TimeRemaining/60;
+        int seconds = TimeRemaining%60;
+        if(on && seconds >= 10) TimeCount.text = minutes.ToString() + ":" + seconds.ToString();
+        else if (on) TimeCount.text = minutes.ToString() + ":0" + seconds.ToString();
+        else TimeCount.text = "0:00";
+        if(TimeRemaining == 0) on = false;
+    }
     void Update()
     {
-        if (Input.GetMouseButtonDown (0)) 
-        {
-            Debug.Log("Press");
-            RaycastHit hitInfo = new RaycastHit ();
-            if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) 
+        if(on){
+            if (Input.GetMouseButtonDown (0)) 
             {
-                Debug.Log ("Object Hit is " + hitInfo.collider.name);
-                if (hitInfo.collider.name == "Circle.001"){
-                    FireAlarmHandle.GetComponent<Animator>().Play("Pull Handle");
-                    AlarmSound.SetActive(true);
-                    message.text = "You have now activated the fire alarm. Now find the fire extinguisher and tap on it to hold it.";
-                }
-                if (hitInfo.collider.gameObject.name == "Fire Extinguisher")
+                Debug.Log("Press");
+                RaycastHit hitInfo = new RaycastHit ();
+                if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) 
                 {
-                    FireExtinguisher.transform.parent = Cam.transform;
-                    FireExtinguisher.transform.position = new Vector3(Cam.transform.position.x - 1.5f, Cam.transform.position.y - 1.6f ,Cam.transform.position.z - 8.5f);
-                    // FireExtinguisher.GetComponent<Animator>().Play("Bring Extinguisher to Camera");
-                    ExtinguisherInFrontOfCamera = true;
-                    FireExtinguisher.layer = LayerMask.NameToLayer("Ignore Raycast");
-                    message.text = "Now tap on the pin to pull it out. This will break the tamper seal";
-                }
+                    Debug.Log ("Object Hit is " + hitInfo.collider.name);
+                    if (hitInfo.collider.name == "Circle.001" && !fireAlarmActivated){
+                        FireAlarmHandle.GetComponent<Animator>().Play("Pull Handle");
+                        fireAlarmActivated = true;
+                        AlarmSound.SetActive(true);
+                        message.text = "You have now activated the fire alarm. Now find the fire extinguisher and tap on it to hold it.";
+                    }
+                    if (hitInfo.collider.gameObject.name == "Fire Extinguisher" && fireAlarmActivated)
+                    {
+                        FireExtinguisher.transform.parent = Cam.transform;
+                        FireExtinguisher.transform.position = new Vector3(Cam.transform.position.x - 1.5f, Cam.transform.position.y - 1.6f ,Cam.transform.position.z - 8.5f);
+                        // FireExtinguisher.GetComponent<Animator>().Play("Bring Extinguisher to Camera");
+                        ExtinguisherInFrontOfCamera = true;
+                        FireExtinguisher.layer = LayerMask.NameToLayer("Ignore Raycast");
+                        message.text = "Now tap on the pin to pull it out. This will break the tamper seal";
+                    }
 
-                if (hitInfo.collider.name == "polySurface14" & ExtinguisherInFrontOfCamera)
-                {
-                    FireExtinguisher.GetComponent<Animator>().Play("Remove the Pin");
-                    PinIsRemoved = true;
-                    FireExtinguisher.transform.Find("polySurface10").gameObject.layer = LayerMask.NameToLayer("Default");
-                    message.text = "Now tap on the pipe and take aim. Point the nozzle at the base of the fire";
-        
-                }
+                    if (hitInfo.collider.name == "polySurface14" & ExtinguisherInFrontOfCamera)
+                    {
+                        FireExtinguisher.GetComponent<Animator>().Play("Remove the Pin");
+                        PinIsRemoved = true;
+                        FireExtinguisher.transform.Find("polySurface10").gameObject.layer = LayerMask.NameToLayer("Default");
+                        message.text = "Now tap on the pipe and take aim. Point the nozzle at the base of the fire";
+            
+                    }
 
-                if (hitInfo.collider.name == "polySurface10" & PinIsRemoved & !aimed)
-                {
-                    Destroy (FireExtinguisher.transform.Find("polySurface14").gameObject);
-                    FireExtinguisher.GetComponent<Animator>().Play("Point Pipe Towards Fire");
-                    aimed = true;
-                    Destroy(FireExtinguisher.transform.Find("polySurface10").gameObject.GetComponent<Collider>());
-                    Destroy(FireExtinguisher.GetComponent<Collider>());
-                    message.text = "Now press and hold the handle to release the extinguishing agent until the fire is put out";
-                }
+                    if (hitInfo.collider.name == "polySurface10" & PinIsRemoved & !aimed)
+                    {
+                        Destroy (FireExtinguisher.transform.Find("polySurface14").gameObject);
+                        FireExtinguisher.GetComponent<Animator>().Play("Point Pipe Towards Fire");
+                        aimed = true;
+                        Destroy(FireExtinguisher.transform.Find("polySurface10").gameObject.GetComponent<Collider>());
+                        Destroy(FireExtinguisher.GetComponent<Collider>());
+                        message.text = "Now press and hold the handle to release the extinguishing agent until the fire is put out";
+                    }
 
-            } 
-        }        
-        else if (Input.GetMouseButton(0)){
-            RaycastHit hitInfo = new RaycastHit ();
-            if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
-                if ((hitInfo.collider.gameObject.name == "polySurface36" | hitInfo.collider.gameObject.name == "polySurface37") & aimed)
-                {
-                    Debug.Log("Fireeee");
-                    FireExtinguisher.GetComponent<Animator>().Play("Pressing Handle");
-                    FireExtinguisher.transform.Find("Smoke").gameObject.SetActive(true);
-                    firing = true;
+                } 
+            }        
+            else if (Input.GetMouseButton(0) && aimed){
+                RaycastHit hitInfo = new RaycastHit ();
+                if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
+                    if ((hitInfo.collider.gameObject.name == "polySurface36" | hitInfo.collider.gameObject.name == "polySurface37") && aimed)
+                    {
+                        Debug.Log("Fireeee");
+                        FireExtinguisher.GetComponent<Animator>().Play("Pressing Handle");
+                        FireExtinguisher.transform.Find("Smoke").gameObject.SetActive(true);
+                        firing = true;
+                    }
+                    
                 }
+            
                 
             }
-        
-            
-        }
 
-        else if (Input.GetMouseButtonUp(0)){
-            if (firing){
-                FireExtinguisher.GetComponent<Animator>().Play("UnPressing Handle");
-                FireExtinguisher.transform.Find("Smoke").gameObject.SetActive(false);
-                firing = false;
-                AlarmSound.SetActive(false);
-                message.text = "Congratulations! You have put out the fire";
+            else if (Input.GetMouseButtonUp(0) && aimed){
+                if (firing){
+                    FireExtinguisher.GetComponent<Animator>().Play("UnPressing Handle");
+                    FireExtinguisher.transform.Find("Smoke").gameObject.SetActive(false);
+                    firing = false;
+                    AlarmSound.SetActive(false);
+                    message.text = "Congratulations! You have put out the fire";
+                }
+
+
             }
-
-
+        }
+        else{
+            message.text = "You have run out of time. Click on Reset to try again";
         }
 
         // if (isFiring) 
