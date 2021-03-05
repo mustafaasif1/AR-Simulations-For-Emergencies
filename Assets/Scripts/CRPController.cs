f7624520317d345d101ff082968aef2988336fb9
Maroof Saleemi <Maroof@saleemis-mbp.home>
2021-03-05 17:39:20 +0500

    CPR done

--- Assets/Scripts/CRPController.cs
+++ Assets/Scripts/CRPController.cs
@@ -26,13 +26,16 @@ public class CRPController : MonoBehaviour
     public bool gameOn;
     public bool compressionDone;
     public bool mouthToMouth;
+    public bool listened;
+    public GameObject female;
+    public bool allowed;
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        message.text = "Step 1: The person is lying on the floor. Tap on the hands to bring them together in order to perform CPR!";
+        message.text = "The person is lying on the floor. Tap on the nose to check for his breath";
         StartingScene = SceneManager.GetActiveScene();
         Reset.onClick.AddListener(ResetClick);
         Shout.onClick.AddListener(ShoutClick);
@@ -42,13 +45,17 @@ public class CRPController : MonoBehaviour
         firstTime = false;
         counter = 0;
         Hands = GameObject.Find("Hands");
+        Hands.SetActive(false);
         male = GameObject.Find("Male_1");
+        female = GameObject.Find("Female");
         TimeRemaining = 20;
         TimeTrack = 20;
         TimeCount.gameObject.SetActive(false);
         gameOn = true;
         compressionDone = false;
         mouthToMouth = false;
+        listened = false;
+        allowed = true;
     }
 
 
@@ -63,6 +70,7 @@ public class CRPController : MonoBehaviour
             int minutes = TimeRemaining/60;
             int seconds = TimeRemaining%60;
             if (TimeRemaining < 10){
+                
                 TimeCount.color = new Color32(255,0,0,255);
             } else{
                 TimeCount.color = new Color32(0, 255, 0,255);
@@ -99,20 +107,36 @@ public class CRPController : MonoBehaviour
                     if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) 
                     {
                         Debug.Log ("Object Hit is " + hitInfo.collider.gameObject.name);
-                        if (hitInfo.collider.gameObject.name == "HandMesh" & !handsTogether){
+                        if (hitInfo.collider.gameObject.name == "HandMesh" & !handsTogether & listened){
                             Hands.GetComponent<Animator>().Play("Put Hands Together");
                             handsTogether = true;
                             message.text = "Well Done! Now tap on the button to beign the CPR";
                             Mover.gameObject.SetActive(true);
                         }
-                        if (hitInfo.collider.gameObject.name == "FaceMesh" & mouthToMouth){
-                            //disable Hands
-                            //play animation of giving mouth to mouth
+                        if (hitInfo.collider.gameObject.name == "FaceMesh" & !listened){
+                            female.GetComponent<Animator>().Play("Female Listening on Breathing");
+                            StartCoroutine(waiter());
+                            
                         }
 
                     }
                 }
         }
+    }
+
+
+    IEnumerator waiter(){
+        yield return new WaitForSeconds(8);
+        Hands.SetActive(true);
+        female.SetActive(false);
+        listened = true;
+        message.text = "We felt no breath on our hand. Now, press on the hands to bring them together and give a CPR";
+        
+    }
+
+    IEnumerator waitForCPR(){
+        yield return new WaitForSeconds(0.3f);
+        allowed = true;
     }
 
     void ResetClick(){
@@ -123,25 +147,33 @@ public class CRPController : MonoBehaviour
     	Debug.Log ("Hello");
     }
     void HandsMover(){
-
-        Hands.GetComponent<Animator>().Play("Compress Hands");
-        male.GetComponent<Animator>().Play("Chest shaking");
+        if (allowed){
+            Hands.GetComponent<Animator>().Play("Compress Hands");
+            male.GetComponent<Animator>().Play("Chest shaking");
+        }
         if (!firstTime){
             firstTime = true;
             message.text = "Excellent! Now keep on tapping the same button to compress the chest 30 times in 20 seconds";
             TimeCount.gameObject.SetActive(true);
         }
-        else if (firstTime){
+        else if (firstTime & allowed){
             counter ++;
-            message.text = "                           " + counter;
-
+            message.text = counter.ToString();
+            allowed = false;
+            StartCoroutine(waitForCPR());
+            
         }
 
         if(counter > 29){
-            message.text = "Well Done! It is time to breath air into the mouth of the person! Tap on the mouth to do so.";
+            message.text = "Well Done! The Person is Now able to breath again thanks to you!";
+            Hands.SetActive(false);
             compressionDone = true;
             mouthToMouth = true;
+            gameOn = false;
+            Mover.gameObject.SetActive(false);
             counter = 0;
+            male.GetComponent<Animator>().Play("Waking Up");
+       
         }
         
             
