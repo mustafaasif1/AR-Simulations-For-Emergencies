1ebd48fb3328c83d5143ffdb298f245c7f1118d7
Maroof Azeem <maroofsaleemi@macs-mbp.home>
2021-04-03 19:40:11 +0500

    New Animations

--- Assets/PersonCatchingFireController.cs
+++ Assets/PersonCatchingFireController.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.EventSystems;
+using GoogleARCore;
 
 public class PersonCatchingFireController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject Person;
+    private GameObject Fire;
+    public Button ActionButton;
+    public Button Reset;
+    public Scene StartingScene;
+    
     void Start()
     {
+        StartingScene = SceneManager.GetActiveScene();
+        Person = GameObject.Find("ManOnFire");
+        Fire = Person.transform.Find("PS_Parent").gameObject;
+        ActionButton.gameObject.SetActive(true);
+        Reset.onClick.AddListener(ResetClick);
+        ActionButton.onClick.AddListener(Faller);
         
     }
 
@@ -14,5 +31,15 @@ public class PersonCatchingFireController : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    void ResetClick(){
+    	SceneManager.LoadScene(StartingScene.name);
+    }
+    
+    void Faller(){
+        Debug.Log("AHHH");
+        Person.GetComponent<Animator>().SetBool("FallDown", true);
     }
 }
--- Assets/Scripts/PersonCatchingFire.cs
+++ Assets/Scripts/PersonCatchingFire.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PersonCatchingFire : MonoBehaviour
 {
+    private GameObject Person;
+    private GameObject Fire;
     // Start is called before the first frame update
     void Start()
     {
-
+        Person = GameObject.Find("ManOnFire");
+        Person.SetActive(false);
+        Fire = Person.transform.Find("PS_Parent").gameObject;
+        Fire.SetActive(true);
+        Person.GetComponent<Animator>().Play("ManOnFire");
     }
 
     // Update is called once per frame
