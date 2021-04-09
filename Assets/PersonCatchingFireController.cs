7f1a01ec0f56062ada62d18d24c00a2f462b9f8a
MaroofSaleemi <maroofazeem@hotmail.com>
2021-04-10 00:14:11 +0500

    left roll

--- Assets/PersonCatchingFireController.cs
+++ Assets/PersonCatchingFireController.cs
@@ -16,6 +16,7 @@ public class PersonCatchingFireController : MonoBehaviour
     public Button Reset;
     public Scene StartingScene;
     public int rolledTimes;
+    public bool letsRoll;
     
     void Start()
     {
@@ -26,6 +27,7 @@ public class PersonCatchingFireController : MonoBehaviour
         Reset.onClick.AddListener(ResetClick);
         ActionButton.onClick.AddListener(onFire);
         rolledTimes = 0;
+        letsRoll = true;
         
     }
 
@@ -42,37 +44,66 @@ public class PersonCatchingFireController : MonoBehaviour
     }
     
     void onFire(){
-        Person.GetComponent<Animator>().Play("In Agony");
-        Fire.SetActive(true);
-        ActionButton.onClick.RemoveListener(onFire);
-        
-        ActionButton.onClick.AddListener(Faller);
-        ActionButton.GetComponentInChildren<TextMeshProUGUI>().text = "Fall Down";
-        
+        if(letsRoll){
+            letsRoll = false;
+            Person.GetComponent<Animator>().Play("In Agony");
+            Fire.SetActive(true);
+            ActionButton.onClick.RemoveListener(onFire);
+            StartCoroutine(WaitForFire());
+            ActionButton.onClick.AddListener(Faller);
+            ActionButton.GetComponentInChildren<TextMeshProUGUI>().text = "Fall Down";
+        }
     }
 
     void Faller(){
-        Person.GetComponent<Animator>().Play("Falling Person");
-        ActionButton.onClick.RemoveListener(Faller);
-        ActionButton.onClick.AddListener(Roller);
-        ActionButton.GetComponentInChildren<TextMeshProUGUI>().text = "Roll";
-        
+        if(letsRoll){
+            letsRoll = false;
+            StartCoroutine(WaitForFire());
+            Person.GetComponent<Animator>().Play("Falling Person");
+            ActionButton.onClick.RemoveListener(Faller);
+            ActionButton.onClick.AddListener(Roller);
+            ActionButton.GetComponentInChildren<TextMeshProUGUI>().text = "Roll";
+        }
     }
+
+    IEnumerator WaitForFire(){
+    yield return new WaitForSeconds(3.0f);
+    letsRoll = true;
+    }
+
+    IEnumerator WaitForRoll(){
+    yield return new WaitForSeconds(1.2f);
+    letsRoll = true;
+    }
+
     void Roller(){
-        Person.GetComponent<Animator>().Play("Rolling Person");
-        rolledTimes ++;
-        if (rolledTimes > 3){
+        if(letsRoll && rolledTimes%2 == 0){
+            letsRoll = false;
+            Person.GetComponent<Animator>().Play("Rolling Person");
+            StartCoroutine(WaitForRoll());
+            rolledTimes ++;
+        }
+        if(letsRoll && rolledTimes%2 == 1){
+            letsRoll = false;
+            Person.GetComponent<Animator>().Play("Rolling Person Opposite");
+            StartCoroutine(WaitForRoll());
+            rolledTimes ++;
+        }
+        if (rolledTimes > 8){
+            
             ActionButton.onClick.RemoveListener(Roller);
             ActionButton.onClick.AddListener(Stander);
             ActionButton.GetComponentInChildren<TextMeshProUGUI>().text = "Stand Up";
-        
+            
         }   
 
     }
 
     void Stander(){
-        Person.GetComponent<Animator>().Play("Standing Person");
-        ActionButton.onClick.RemoveListener(Stander);
+        if(letsRoll){
+            Person.GetComponent<Animator>().Play("Standing Person");
+            ActionButton.onClick.RemoveListener(Stander);
+        }
             
     }
 }
